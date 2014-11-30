aa9e90832d2e39974e9cc4c60df7f82b758bf53b
Hidetake Fuse <hidetake.enpit@gmail.com>
2014-11-30 23:17:38 +0900

    内部処理

--- app/src/main/java/com/cookhat/cookbeauty/AnimationFragment.java
+++ app/src/main/java/com/cookhat/cookbeauty/AnimationFragment.java
@@ -1,13 +1,14 @@
 package com.cookhat.cookbeauty;
 
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +26,7 @@ import java.util.Map;
 public class AnimationFragment extends Fragment{
     private  DBHelper mDbHelper;
     private SQLiteDatabase db;
+    private FrameActivity frame;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -34,15 +36,24 @@ public class AnimationFragment extends Fragment{
         container.removeAllViews();
 
         mDbHelper = new DBHelper(this.getActivity().getApplicationContext());
-        mDbHelper.createEmptyDataBase(); //DB更新
-        db = mDbHelper.getReadableDatabase();
-        ContentValues v = new ContentValues();
 
+        mDbHelper.createEmptyDataBase(); //DB更新
+
+        db = mDbHelper.getReadableDatabase();
+         /*テストコード*/
+        /*ここまで*/
+        ContentValues v = new ContentValues();
+/*
         v.put("name","きむりいのおにぎり");
         String whereClause = "id = ?";
         String whereArgs[] = new String[1];
         whereArgs[0] = "1";
         db.update("table_recipeLists",v,whereClause,whereArgs);
+*/
+        mDbHelper.calcRecommend();
+
+
+
         View vi =inflater.inflate(R.layout.list_activity, container, false);
 
         if (db != null) {
@@ -52,26 +63,29 @@ public class AnimationFragment extends Fragment{
             int size = 0;
 
 
-            Map<Integer, Map> columus = mDbHelper.findAll("table_recipeLists", 0, 0);
-            size = columus.size();
+            Map<Integer, Map> columns = mDbHelper.findAll("table_recipeLists", 0, 0);
+            size = columns.size();
 
             String name[] = new String[size];
             int id_number[] = new int[size];
             int key = 0;
             final int[] listOrder = new int[size];
-            Iterator iterator = columus.keySet().iterator();
+            Iterator iterator = columns.keySet().iterator();
             while (iterator.hasNext()) {
                 Object o = iterator.next();
                 //rowData = columus.get(o);
-                name_buf = (String) columus.get(o).get("name");
-                key = Integer.parseInt((String) columus.get(o).get("id"));
+                name_buf = (String)columns.get(o).get("name");
+                key = Integer.parseInt((String) columns.get(o).get("id"));
                 Log.v("料理名", name_buf);
-                Log.v("ジャンル", (String) columus.get(o).get("genre"));
-                Log.v("甘味", (String) columus.get(o).get("amami"));
-                Log.v("塩味", (String) columus.get(o).get("shio"));
-                Log.v("旨味", (String) columus.get(o).get("umami"));
-                Log.v("酸味", (String) columus.get(o).get("acid"));
-                Log.v("辛味", (String) columus.get(o).get("pain"));
+                Log.v("ジャンル", (String) columns.get(o).get("genre"));
+                Log.v("甘味", (String) columns.get(o).get("amami"));
+                Log.v("塩味", (String) columns.get(o).get("shio"));
+                Log.v("旨味", (String) columns.get(o).get("umami"));
+                Log.v("酸味", (String) columns.get(o).get("acid"));
+                Log.v("辛味", (String) columns.get(o).get("pain"));
+                Log.v("レート", (String) columns.get(o).get("rating"));
+                Log.v("オススメ度", (String) columns.get(o).get("recommend"));
+                Log.v("調理済みフラグ", (String) columns.get(o).get("cooked"));
 
                 id_number[key - 1] = key;
 
@@ -97,15 +111,15 @@ public class AnimationFragment extends Fragment{
             // Adapterのインスタンス化
             // 第三引数にlabelListを渡す
             CustomAdapter mAdapter = new CustomAdapter(this.getActivity().getApplicationContext(), 0, labelList);
-            Log.v("test", "カスタム");
+
 
             // リストにAdapterをセット
             list.setAdapter(mAdapter);
 
-            Log.v("test", "リストセット");
+
             // リストアイテムの間の区切り線を非表示にする
             list.setDivider(null);
-            Log.v("test", "divider");
+
             // アイテムクリック時ののイベントを追加
             list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent,
@@ -117,17 +131,27 @@ public class AnimationFragment extends Fragment{
                     // 表示列の料理IDの取得
                     int no = listOrder[pos];
                     //Log.v("test", "test:"+String.valueOf(test));
+                    frame.changeRecipeFragment(no);
 
-                    // 画面起動
+
+                   /*Activity使ってた頃のコード
+
                     Intent intent = new Intent(getActivity(), RecipeActivity.class);
                     intent.putExtra("id", no);
+                    startActivity(intent);*/
 
-                    startActivity(intent);
+
                 }
             });
         }
-        Log.v("test", "before return");
+
         return vi;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        frame = (FrameActivity) activity;
+        super.onAttach(activity);
     }
 }
 
